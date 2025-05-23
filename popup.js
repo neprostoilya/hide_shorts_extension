49a156443efa96c75811fc790178298032e0f86b
Ilya Chaplenko <125191093+neprostoilya@users.noreply.github.com>
2025-05-23 15:49:06 +0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     chrome.storage.sync.get(['shortsHidden'], function(result) {
         const isEnabled = result.shortsHidden || false;
         toggleBtn.checked = isEnabled;
-        
+         
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             const tab = tabs[0];
             if (tab && tab.url.includes("https://www.youtube.com/")) {
